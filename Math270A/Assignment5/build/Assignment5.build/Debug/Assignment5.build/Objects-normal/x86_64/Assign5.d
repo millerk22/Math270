523ac0590d0d8b5d0074fece1d28de786110b459
Kevin Miller <ksmill327@gmail.com>
2017-11-07 12:31:53 -0800

    Final stuff deleted

--- Math270A/Assignment5/build/Assignment5.build/Debug/Assignment5.build/Objects-normal/x86_64/Assign5.d
+++
@@ -1,7 +0,0 @@
-dependencies: \
-  /Users/Kevin/Desktop/UCLA2017/Math270/Math270A/Assignment5/Assignment5/Assign5.cpp \
-  /Users/Kevin/Desktop/UCLA2017/Math270/Math270A/Assignment4/Assignment4/GridFun2D.h \
-  /Users/Kevin/Desktop/UCLA2017/Math270/Math270A/Assignment4/Assignment4/DoubleArray2D.h \
-  /Users/Kevin/Desktop/UCLA2017/Math270/Math270A/Assignment4/Assignment4/RelaxOp2D.h \
-  /Users/Kevin/Desktop/UCLA2017/Math270/Math270A/Assignment3/Assignment3/Assignment3/TriSolver.h \
-  /Users/Kevin/Desktop/UCLA2017/Math270/Math270A/Assignment4/Assignment4/GNUplotUtility.h
