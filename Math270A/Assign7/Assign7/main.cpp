05d0250a6d714e2c954655c1dc23583cc1305911
Kevin Miller <ksmill327@gmail.com>
2017-11-21 16:21:03 -0800

    Added assignment 7 code stuff plus updated notes

---
+++ Math270A/Assign7/Assign7/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assign7
+//
+//  Created by Kevin Miller on 11/20/17.
+//  Copyright © 2017 Kevin Miller. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
