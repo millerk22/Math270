d1a8d0c0e2021ccd24d963ff5bee546f126e3d00
Kevin Miller <ksmill327@gmail.com>
2017-12-06 15:31:07 -0800

    Changes to 266.py

--- Math270A/266.py
+++ Math270A/266.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 
 nf = raw_input("Please input range for q,r (nf):")
-num_gridpoints = 2001
+num_gridpoints = 501
 
 q = np.linspace(-nf,nf,num_gridpoints)
 r = np.linspace(-nf,nf,num_gridpoints)
@@ -13,9 +13,11 @@ r = np.linspace(-nf,nf,num_gridpoints)
 Roots = np.zeros((num_gridpoints, num_gridpoints))
 zeros = 0
 for i in xrange(num_gridpoints):
+    if i % 50 == 0:
+        print i
     for j in xrange(num_gridpoints):
         roots = np.roots(np.array([-r[j], q[i]*r[j], -(q[i]+r[j]), q[i]*r[j]]))
-        if np.sum(np.abs(np.imag(roots))) > .0001:
+        if np.sum(np.abs(np.imag(roots))) > .00001:
             Roots[i,j] = -1.
         elif len(roots) == 1:
             zeros += 1
@@ -24,5 +26,5 @@ for i in xrange(num_gridpoints):
 
 plt.imshow(Roots.T[:,::-1], cmap='spectral')
 plt.colorbar()
-plt.savefig('RootsPlot.png')
+plt.savefig('RootsPlot_'+str(nf)+'.png')
 plt.show()
